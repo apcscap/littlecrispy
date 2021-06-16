d82402be8ea89fd1b143e39c6b66aeabbcbb0035
calee14 <caplee1000@gmail.com>
2021-06-16 15:04:37 -0700

    add fade opacity

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,10 @@ const Card = (props) => {
         <>
         <div className="card-container">
             <div className="card" onMouseEnter={() => {onHovered(true)}} onMouseLeave={() => onHovered(false)}>
-                <div className={`img-container ${isHovered && 'fade-out'}`}>
+                <div className={`img-container ${isHovered && 'no-opacity'}`}>
                     <img className={`card-img`} src={sage}/>
                 </div>
-                <p className={`letter ${!isHovered ? 'hide' : 'fade-in'}`}>{`${isHovered}`}</p>
+                <p className={`letter ${!isHovered ? 'hide' : 'full-opacity'}`}>{`${isHovered}`}</p>
             </div>
         </div>
         </>
