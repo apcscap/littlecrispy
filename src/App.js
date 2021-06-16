c5600bd70235c16eebc48294a41a5cd81e1f5fc9
calee14 <caplee1000@gmail.com>
2021-06-16 11:11:46 -0700

    add state for hover

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 import Header from './components/Header';
 import Row from './components/Row';
 import Card from './components/Card';
+import {useState} from 'react';
 
 function App() {
+
+  const [isHovered, onHovered] = useState(false);
+
+  const hover = (val) => {
+    onHovered(val);
+  }
+
   return (
     <div className="container">
       <Header/>
       <Row>
-        <Card/>
-        <Card/>
+        <Card title="Hello" text="Candice" onHover={hover}/>
+        <Card title="Hello" text="Candice" onHover={hover}/>
       </Row>
       <Row>
-        <Card/>
-        <Card/>
+        <Card title="Hello" text="Candice" onHover={hover}/>
+        <Card title="Hello" text="Candice" onHover={hover}/>
       </Row>
     </div>
   );
