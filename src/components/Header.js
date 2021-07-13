bfd673f3d20069c3a695e4f5af80d44c9541e73d
calee14 <caplee1000@gmail.com>
2021-07-13 16:53:17 -0700

    move background img with mouse move

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 const Header = () => {
+    const [mousePos, setMousePos] = useState({left: 0, top: 0})
+    
+    const handleMouseMove = (e) => {
+        setMousePos({left: e.nativeEvent.offsetX, top: e.nativeEvent.offsetY})
+        e.currentTarget.style.backgroundPositionX = -150 -(mousePos.left)*0.05 + "px";
+        e.currentTarget.style.backgroundPositionY = -100 -(mousePos.top)*0.01 + "px";
+    }
+
     return (
         <>
-        <h1 className="sage-title">Courtney "Sage" Wong</h1>
+        <h1 onMouseMove={handleMouseMove} className="sage-title">Courtney "Sage" Wong</h1>
         <h2 className="title-center">THE SMART ONE</h2>
         </>
     )
