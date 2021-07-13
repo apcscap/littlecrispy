1a2d58120b884f73eb9e24e0a4b46976e841d322
calee14 <caplee1000@gmail.com>
2021-07-12 23:52:43 -0700

    add button comp to app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import Row from './components/Row';
 import Card from './components/Card';
-import Surprise from './components/Surprise'
+import Surprise from './components/Surprise';
+import Button from './components/Button';
 import {useState} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
@@ -30,7 +31,7 @@ function App() {
               <Card title="Hello" text="Candice" onHover={hover}/>
             </Row>
             <p style={{textAlign: 'center', fontStyle: 'normal', }}>If your name is Courtney "Sage" Wong you can claim this website. Must show proof of identical name with birth certificate.</p>
-            <Link to='/surprise'>Click here for your radiant points</Link>
+            <Link to='/surprise'><Button>Click here for your radiant points</Button></Link>
           </>
         )}/>
         <Route path='/surprise' component={Surprise}/>
