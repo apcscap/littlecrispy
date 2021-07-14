45f7136e55ceba31631a248f8ec3669d3d6d2e20
calee14 <caplee1000@gmail.com>
2021-07-13 17:00:51 -0700

    comm mouseMove event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 
 const Header = () => {
-    const [mousePos, setMousePos] = useState({left: 0, top: 0})
+    const [mouseChange, setMouseChange] = useState({left: 0, top: 0})
     
     const handleMouseMove = (e) => {
-        setMousePos({left: e.nativeEvent.offsetX, top: e.nativeEvent.offsetY})
-        e.currentTarget.style.backgroundPositionX = -150 -(mousePos.left)*0.05 + "px";
-        e.currentTarget.style.backgroundPositionY = -100 -(mousePos.top)*0.01 + "px";
+        // update mouseChange with the shift in the mouse
+        setMouseChange({left: e.nativeEvent.offsetX, top: e.nativeEvent.offsetY})
+        // e.currentTarget is the element that is calling handleMouseMove
+        // update the position of the image with a shift based on the mouseChange
+        e.currentTarget.style.backgroundPositionX = -150 -(mouseChange.left)*0.05 + "px";
+        e.currentTarget.style.backgroundPositionY = -100 -(mouseChange.top)*0.01 + "px";
     }
 
     return (
