5700c898a5b193618217c2851665835b3ac506db
calee14 <caplee1000@gmail.com>
2021-07-14 12:52:42 -0700

    add vid op for card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Card = (props) => {
-    const {title, text, children, imgPath} = props;
+    const {title, text, children, imgPath, video} = props;
     const [isHovered, onHovered] = useState(false)
 
     return (
@@ -9,9 +9,17 @@ const Card = (props) => {
         <div className="card-container">
             <div className="card" onMouseEnter={() => {onHovered(true)}} onMouseLeave={() => onHovered(false)}>
                 <div className={`img-container ${isHovered && 'no-opacity'}`}>
-                    <img alt="none" className={`card-img`} src={imgPath}/>
+                    { video===false ?
+                        (<img alt="none" className={`card-img`} src={imgPath}/>) :
+                        (<video className="song-vid" autoplay="autoplay" muted="muted" loop="loop">
+                            <source src={imgPath} type="video/mp4"></source>
+                        </video>)
+                    }
                 </div>
-                <p className={`letter ${!isHovered ? 'hide' : 'full-opacity'}`}>{`${isHovered}`}</p>
+                <div className={`letter ${!isHovered ? 'hide' : 'full-opacity'}`}>
+                    {`${isHovered}`}
+                    <h1>Jo Mamma</h1>
+                </div>
             </div>
         </div>
         </>
