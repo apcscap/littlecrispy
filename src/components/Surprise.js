2a83ffcaf518c1fa0b2b315eb4c5f6c1ba327000
calee14 <caplee1000@gmail.com>
2021-07-15 14:10:22 -0700

    add surprise code

--- src/components/Surprise.js
+++ src/components/Surprise.js
@@ -9,7 +9,11 @@ const Surprise = () => {
                 <ReactPlayer className="rollContainer" url='https://www.youtube.com/watch?v=pUvwleVGVmA'
                         playing={true}
                         muted={true}/>
+                <h2 className="rollContainer">Your Val Points</h2>
+                <p className="rollContainer">RA-KPH5XJOMAMMAMAMA</p>
             </div>
+
+            
             
             <Link to='/'><SurpriseButton>Go Back</SurpriseButton></Link>
         </>
